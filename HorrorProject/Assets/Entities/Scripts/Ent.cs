03e91807966f96ff574f2246a9881abbe54e5fe2
Kaelen Cook <kaelen1cook@gmail.com>
2024-05-07 19:38:54 -0700

    added flight
    zooom

--- HorrorProject/Assets/Entities/Scripts/Ent.cs
+++ HorrorProject/Assets/Entities/Scripts/Ent.cs
@@ -7,13 +7,31 @@ public class Ent : MonoBehaviour
     public Vector2 mouseInputVector;
     public Vector3 movementInputVector;
     public GameObject cursor;
+    public GameObject head;
+    public CapsuleCollider pC;
+    public Rigidbody rb;
     public float speed = 10f;
     public float mouseSensitivity = 1000;
     public float cursorDistance;
     public float userSphereCircumpherence;
+    public float xrotation;
+    public float yrotation;
+    protected bool moving = false;
+    protected bool movingForward = false;
+    protected bool movingBackward = false;
+    protected bool movingLeftward = false;
+    protected bool movingRightward = false;
+    protected bool spaceDown = false;
+    protected bool ctrlDown = false;
+    public float accelerationSpeed;
+    public float maxSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        xrotation = transform.rotation.x;
+        yrotation = transform.rotation.y;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         cursorDistance = Vector3.Distance(cursor.transform.position, transform.position);
         userSphereCircumpherence = cursorDistance * 2 * Mathf.PI;
     }
@@ -24,6 +42,12 @@ public class Ent : MonoBehaviour
         Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, speed * Time.deltaTime, 0.0f);
         transform.rotation = Quaternion.LookRotation(newDirection);
         // transform.rotation = Quaternion.RotateTowards(target.transform.rotation, Quaternion.Euler(0, target.transform.rotation.eulerAngles.y, 0), 20);
+    }
+    protected void RotateMouse(GameObject obj)
+    {
+        yrotation += mouseInputVector.x;
+        xrotation -= mouseInputVector.y;
+        obj.transform.rotation = Quaternion.RotateTowards(obj.transform.rotation, Quaternion.Euler(xrotation, yrotation, 0), 20);
     }
     protected void UpdateMouseInput()
     {
@@ -38,17 +62,113 @@ public class Ent : MonoBehaviour
         Vector3 prevMovementVector = movementInputVector;
         movementInputVector = ((horizontalInput * transform.right) + (verticalInput * transform.forward)).normalized;
     }
+    protected void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movingForward = true;
+            moving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            movingForward = false;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movingBackward = true;
+            moving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            movingBackward = false;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movingLeftward = true;
+            moving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            movingLeftward = false;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movingRightward = true;
+            moving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            movingRightward = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceDown = true;
+            moving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            spaceDown = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            ctrlDown = true;
+            moving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            ctrlDown = false;
+        }
+    }
 
+    protected void Move() {
+        if (movingForward)
+        {
+            rb.AddForce(accelerationSpeed * transform.forward, ForceMode.Acceleration);
+        }
+        if (movingBackward)
+        {
+            rb.AddForce(accelerationSpeed * -transform.forward, ForceMode.Acceleration);
+        }
+        if (movingLeftward)
+        {
+            rb.AddForce(accelerationSpeed * -transform.right, ForceMode.Acceleration);
+        }
+        if (movingRightward)
+        {
+            rb.AddForce(accelerationSpeed * transform.right, ForceMode.Acceleration);
+        }
+        if (spaceDown)
+        {
+            rb.AddForce(accelerationSpeed * transform.up, ForceMode.Acceleration);
+        }
+        if (ctrlDown)
+        {
+            rb.AddForce(accelerationSpeed * -transform.up, ForceMode.Acceleration);
+        }
+        if (!movingForward && !movingBackward && !movingLeftward && !movingRightward && !spaceDown && !ctrlDown)
+        {
+            if (rb.velocity == Vector3.zero)
+            {
+                moving = false;
+            }
+            else 
+            {
+                moving = true;
+            }
+        }
+    }
     protected void UpdateCursor(GameObject target)
     {
-        Vector3 newPosition = target.transform.position + new Vector3(mouseInputVector.x, 0, mouseInputVector.y);
-        if (newPosition != transform.position)
-        {
-            RotateTo(newPosition);
-            Vector3 targetDirection = newPosition - transform.position;
-            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, speed * Time.deltaTime, 0.0f);
-            target.transform.position = transform.position + cursorDistance * newDirection;
-        }
+        target.transform.position = transform.forward * 40f;
+        // Vector3 newPosition = target.transform.position + new Vector3(mouseInputVector.x, mouseInputVector.y, 0);
+        // target.transform.RotateAround(transform.position, newPosition, 20 * Time.deltaTime);
+        // if (newPosition != transform.position)
+        // {
+        //     RotateTo(newPosition);
+        //     Vector3 targetDirection = newPosition - transform.position;
+        //     Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, speed * Time.deltaTime, 0.0f);
+        //     target.transform.position = transform.position + cursorDistance * newDirection;
+        // }
     }
 
     // Update is called once per frame
@@ -56,7 +176,14 @@ public class Ent : MonoBehaviour
     {
         UpdateMouseInput();
         UpdateCursor(cursor);
+        RotateMouse(gameObject);
         Debug.DrawLine(transform.position, cursor.transform.position, Color.green);
+        Debug.DrawLine(transform.position, transform.forward * 100000f, Color.green);
+        Debug.DrawLine(head.transform.position, head.transform.forward * 100000f, Color.blue);
         // RotateTo(cursor);
+        HandleInput();
+        if (moving) {
+            Move();
+        }
     }
 }
